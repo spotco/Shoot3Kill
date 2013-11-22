e72c4ac73810d59bb50e8d6b00e4e999cd464329
Carlo Valentin <me@carlovalentin.com>
2013-11-22 04:10:39 -0800

    Integration Begin

--- Assets/multiplayer_client/OnlinePlayerManager.cs
+++ Assets/multiplayer_client/OnlinePlayerManager.cs
@@ -7,86 +7,91 @@ public class OnlinePlayerManager : MonoBehaviour
 {
 		static Hashtable player_table;
 		static Hashtable bullet_table;
-		static string player_name;
-		static int player_id;
+		static Player the_player;
 		static SPMessage global_msg; 
-
+		List<SPBulletObject> player_bullets;
+			
 		void Start ()
 		{
-				player_id = 0;
-				player_name = "player" + player_id;
 				player_table = new Hashtable ();
 				bullet_table = new Hashtable ();
-
-				create_message ();
-		}
-
-		void create_message() {
-				global_msg = new SPMessage ();
-				List<SPPlayerObject> players = new List<SPPlayerObject> ();
-				SPPlayerObject player = new SPPlayerObject ();
-				player._alive = 1;
-				player._id = 1;
-				player._name = "luke";
-
-				Vector3 pos_vector = new Vector3 (0,0,0);
-				Vector3 vel_vector = new Vector3 (0,0,0);
-				Vector3 rot_vector = new Vector3 (0,0,0);
-
-				player._pos = pos_vector;
-				player._vel = vel_vector;
-				player._rot = rot_vector;
-
-				players.Add (player);
-
-				global_msg._players = players;
-
-				List<SPBulletObject> bullets = new List<SPBulletObject> ();
-
-				global_msg._bullets = bullets;
-
+				player_bullets = new List<SPBulletObject>();
 		}
 
 
 		void Update ()
 		{
-				read_message ();
 				update_sprites ();
-				update_message ();
+				send_message ();
+				kill_player ();
 		}
 
-		void update_message(){
-				List<SPPlayerObject> players = global_msg._players;
-				var player = players [0];
-				Vector3 vel_vector = new Vector3 (0.05f,0f,0f);
-				player._vel = vel_vector;
+		void send_new_player(string newName){
+				SPPlayerObject myPlayer = new SPPlayerObject ();
+				myPlayer._id = -1;
+				myPlayer._name = newName;
+				myPlayer._pos = new Vector3 (0, 0, 0);
+				myPlayer._vel = new Vector3 (0, 0, 0);
+				myPlayer._rot = new Vector3 (0, 0, 0);
+				myPlayer._alive = 1;
+
+				SPMessage new_message = new SPMessage ();
+
+				new_message._players.Add(myPlayer);
+
+				//socketmagicfuckerysendtoserver (new_message);
 		}
 
-		void read_message() {
-				SPMessage msg = global_msg; // Mock, will be incoming from server
-				foreach (SPPlayerObject player_msg in msg._players) {
+		void create_myself(SPMessage c_section){
+				SPPlayerObject player_obj = (SPPlayerObject)c_section._players [0]; 
+				Player thine_player = new Player (player_obj);
+				thine_player._player_object = gameObject.GetComponent<RigidBody> ();
+				the_player = thine_player;
+		}
+
+		void create_bullet(SPBulletObject new_bullet){
+				player_bullets.add (new_bullet);
+		} 
+
+
+		void read_message (SPMessage new_message) {
+				foreach (SPPlayerObject player_msg in new_message._players) {
 						int id = player_msg._id;
 						if (!player_table.ContainsKey (id)) {
 								Player new_player = new Player (player_msg);
 								player_table.Add (id, new_player);
 						} else {
 								if (player_msg._alive == 0) {
-										destroy_player ((Player)player_table[id]);
+										destroy_player ((Player)player_table [id]);
 								} else {
 										update_player (id, player_msg);
 								}
 						}
 				}
 
-				foreach (SPBulletObject bullet_msg in msg._bullets) {
-						int bul_id = bullet_msg._id;
+				foreach (SPBulletObject bullet_msg in new_message._bullets) {
+						string bul_id = bullet_msg._id;
 						if (!bullet_table.ContainsKey (bul_id)) {
 								Bullet new_bullet = new Bullet (bullet_msg);
 								bullet_table.Add (bul_id, new_bullet);
 						} else {
 								update_bullet (bul_id, bullet_msg);
 						}
+
+						if (collision ((Player)the_player, (Bullet)bullet_table [bul_id])) {
+								kill_player ();
+						}
 				}
+		}
+	
+		SPMessage send_message(){
+				SPMessage new_message = new SPMessage ();
+
+				new_message._players.Add(the_player.convert_to_obj());
+
+				new_message._bullets = player_bullets;
+
+				return new_message; 
 		}
 
 		void update_sprites () {
@@ -95,7 +100,6 @@ public class OnlinePlayerManager : MonoBehaviour
 						if (p.get_player_object () == null) {
 								GameObject new_player_object = (GameObject)Instantiate (Resources.Load ("SimulatedPlayer"));
 								p.set_player_object(new_player_object);
-								Debug.Log (new_player_object);
 						} else {
 								p.update ();
 						}
@@ -105,8 +109,7 @@ public class OnlinePlayerManager : MonoBehaviour
 						if (b.get_bullet_object () == null) {
 								//GameObject new_player_object = (GameObject)Instantiate (Resources.Load ("SimulatedBullet"));
 						}else{
-
-								if(b.is_out_of_bounds() || collision((Player)player_table[player_id], b)){ 
+								if(b.is_out_of_bounds()){ 
 										destroy_bullet(b);
 								}
 						}
@@ -122,6 +125,14 @@ public class OnlinePlayerManager : MonoBehaviour
 				return false;
 		}
 
+		void kill_player(){
+				Player myPlayer = the_player;
+				player_table.Remove (the_player._id);
+				myPlayer._alive = 0;
+				destroy_player (myPlayer);
+				myPlayer.respawn();
+				player_table.Add (the_player._id, myPlayer);
+		}
 
 		void destroy_player(Player player) {
 				GameObject.Destroy (player.get_player_object ());
@@ -129,7 +140,6 @@ public class OnlinePlayerManager : MonoBehaviour
 		}
 
 		void destroy_bullet (Bullet bullet){
-
 				GameObject.Destroy (bullet.get_bullet_object ());
 				bullet.set_bullet_object (null);
 		}
@@ -141,12 +151,11 @@ public class OnlinePlayerManager : MonoBehaviour
 				player_to_update._rotation = msg._rot;
 		}
 
-		void update_bullet (int id, SPBulletObject bullet_msg)
+		void update_bullet (string id, SPBulletObject bullet_msg)
 		{
 				Bullet bullet_to_update = (Bullet) bullet_table [id];
 				bullet_to_update._bul_pos = bullet_msg._pos;
 				bullet_to_update._bul_vel = bullet_msg._vel;
 				bullet_to_update._bul_rotation = bullet_msg._rot;
 		}
-
 }
