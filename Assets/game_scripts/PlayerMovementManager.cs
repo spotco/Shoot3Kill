1264ee4e794ecb45ebc5537398139d3266039011
spotco <mootothemax@gmail.com>
2013-11-29 22:46:02 -0800

    bullet hitting works

--- Assets/game_scripts/PlayerMovementManager.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-using System.Collections;
-using System;
-
-public class PlayerMovementManager {
-
-}
--- Assets/net/server/make.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-rm Shoot3KillServer.exe
-gmcs /define:SERVER Shoot3KillServer.cs ../../util/IOut.cs ../SPDefs.cs ../../util/JSONObject.cs SocketPolicyServer.cs IUtil.cs
