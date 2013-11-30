a26595c9efa0b20a54eee10e6dda8b4a15f294d2
spotco <mootothemax@gmail.com>
2013-11-30 03:27:12 -0800

    nametag work, todo write

--- Assets/game_scripts/net/PlayerInfo.cs
+++ Assets/game_scripts/net/PlayerInfo.cs
@@ -10,3 +10,16 @@ public class PlayerInfo {
 	public static int _respawn_ct;
 
 }
+
+/**
+ * TODO --
+ * implement name login + name over head
+ * better motion prediction:
+ * 		more lastframe polls on client side
+ * 		smooth interpolation to msg recieved point client side
+ * scoreboard
+ * chat board
+ * hud gui with latency and kills/deaths
+ * thread for spbullet timeout on server
+ * particle effects for player death, bullet hit
+ * /
---
+++ Assets/game_scripts/util/FacePlayer.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacePlayer : MonoBehaviour {
+
+	void Update () {
+		gameObject.transform.LookAt(S3KControl.inst.gameObject.transform);
+	}
+}
